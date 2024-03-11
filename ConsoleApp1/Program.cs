00f258d5b9977ac108ffbbe5e4b2b828c07ca605
s26958 <s26958@pjwstk.edu.pl>
2024-03-11 18:27:44 +0100

    Zadanie 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,21 @@
-﻿class Program
+﻿using System.Threading.Tasks.Dataflow;
+
+class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Hi there");
+        int[] tab = { 1, 2, 3, 4, 5 };
+        Srednia(tab);
+    }
+
+    public static void Srednia(int[] tab)
+    {
+        int sum = 0;
+        foreach (int num in tab)
+        {
+            sum = sum + num;
+        }
+        Console.WriteLine(sum/tab.Length);
     }
 }
